03452a6586d8645145a2a01ebd5fee974cbcad8b
davidPope1 <david_1565@yahoo.com>
2024-08-20 15:41:21 +0300

    Installed React Router, added routes, nav links, details link, used route parameters, added the edit activity route and moved the home page outsite of nav bar

--- Persistence/Migrations/20240819132136_InitialCreate.Designer.cs
+++ Persistence/Migrations/20240819132136_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20240806091254_InitialCreate")]
+    [Migration("20240819132136_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
